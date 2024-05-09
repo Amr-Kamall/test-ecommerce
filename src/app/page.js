0506d096642925346797fdb0b7248faf115c637f
amr <amr442028@gmail.com>
2024-05-10 00:09:43 +0300

    update fetching

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,14 @@ import Products from "../../components/products/products";
 import { getNewArrivals } from "../../lib/products";
 
 export default async function Home() {
-  const products = await getNewArrivals();
+  // const products = await getNewArrivals();
 
-  const featuredItem = products.find((item) => item.id === "cqiwhz6jo8");
+  // const featuredItem = products.find((item) => item.id === "cqiwhz6jo8");
   return (
     <div>
-      <Featured featuredItem={featuredItem} />
-      {/* <div suppressHydrationWarning></div> */}
-
-      <Products products={products} proType="newArrivals" />
+      {/* <Featured featuredItem={featuredItem} /> */}
+      {/* <Products products={products} proType="newArrivals" /> */}
+      arrivals
     </div>
   );
 }
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -2,11 +2,12 @@ import Products from "../../../components/products/products";
 import { getAllProducts } from "../../../lib/products";
 
 async function AllProductsPage() {
-  const allProdcuts = await getAllProducts();
-  console.log(allProdcuts);
+  // const allProdcuts = await getAllProducts();
+  // console.log(allProdcuts);
   return (
     <>
-      <Products products={allProdcuts} proType="allProducts" />
+      all products
+      {/* <Products products={allProdcuts} proType="allProducts" /> */}
     </>
   );
 }
