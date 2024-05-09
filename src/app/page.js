bc341dc889d2fab636ce7b1a230d93a98ac8d482
amr <amr442028@gmail.com>
2024-05-09 23:35:53 +0300

    update fetch

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,25 @@ import Featured from "../../components/featured";
 import Products from "../../components/products/products";
 
 export default async function Home() {
-  const res = await fetch("http://localhost:5005/newArrivals");
-  const products = await res.json();
-  console.log(products);
+  try {
+    const res = await fetch("http://localhost:5005/newArrivals");
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const products = await res.json();
+    console.log(products);
 
-  const featuredItem = products.find((item) => item.id === "cqiwhz6jo8");
-  return (
-    <div>
-      <Featured featuredItem={featuredItem} />
-      {/* <div suppressHydrationWarning></div> */}
+    const featuredItem = products.find((item) => item.id === "cqiwhz6jo8");
+    return (
+      <div>
+        <Featured featuredItem={featuredItem} />
+        {/* <div suppressHydrationWarning></div> */}
 
-      <Products products={products} proType="newArrivals" />
-    </div>
-  );
+        <Products products={products} proType="newArrivals" />
+      </div>
+    );
+  } catch (error) {
+    console.log("Error fetching data:", error);
+    // Render some error message or handle the error in a different way
+  }
 }
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -2,6 +2,9 @@ import Products from "../../../components/products/products";
 
 async function AllProductsPage() {
   const res = await fetch("http://localhost:5005/allProducts");
+  if (!res.ok) {
+    throw new Error("failed in fetch data yalaaa");
+  }
   const allProdcuts = await res.json();
   console.log(allProdcuts);
   return (
